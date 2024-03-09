fd4c80306461bf452c3c8c959e42226ed48268f5
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-03-09 08:02:47 +0545

    modified:   .vscode/launch.json modified:   read.c

--- read.c
+++ read.c
@@ -2,13 +2,16 @@
 int main()
 {
     float marks[38],sum=0,avg;
-    
+    int i;
     printf("enter the marks");
-    for(i=0;i<38;i++){
+    for(i=0;i<38;i++)
+    {
     scanf("%f",&marks[i]);
     sum=sum+marks[i];
     }
+
     avg=sum/38;
-    printf("\nAvg=%f",Avg);
+    printf("\nAvg=%f",avg);
     return 0;
+
 }
