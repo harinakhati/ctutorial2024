44c5c275f82d5bde2b5245b08a24c4c6449bb42e
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-03-11 15:33:31 +0545

    read.c read.exe

--- read.c
+++ read.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    float marks[38],sum=0,avg;
+    float marks[5],sum=0,avg;
     int i;
     printf("enter the marks");
-    for(i=0;i<38;i++)
+    for(i=0;i<5;i++)
     {
     scanf("%f",&marks[i]);
     sum=sum+marks[i];
     }
 
-    avg=sum/38;
+    avg=sum/5;
     printf("\nAvg=%f",avg);
     return 0;
 
